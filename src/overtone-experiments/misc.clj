d3dfa689f46abe5851fac7462174b03c4e058abe
fedreg <bksgtr@gmail.com>
2020-01-14 10:40:07 -0600

    Use symbols instead of keywords for notes

--- src/overtone-experiments/misc.clj
+++ src/overtone-experiments/misc.clj
@@ -50,26 +50,6 @@
 (defn make-note
   "Convers a note representation (i.e. :C34) into a map of playable data"
   [n acc]
-  (let [note  (name n)
-        name  (-> note (str/split #"\d") first keyword)
-        nums  (last (str/split note #"\D"))
-        o     (-> nums first str Integer/parseInt)
-        r     (->> nums rest (apply str) Integer/parseInt)
-        oct   (get-octave o)
-        dur   (get-duration r)
-        pitch (+ oct (name hz))
-        time  (if (and @cur-time (not (empty? acc))) (+ (:duration (last acc)) @cur-time) (o/now))]
-    {:name     name
-     :rhythm   r
-     :range    o
-     :duration dur
-     :octave   oct
-     :time     time
-     :pitch    pitch}))
-
-(defn make-note2
-  "Convers a note representation (i.e. :C34) into a map of playable data"
-  [n acc]
   (let [note  (str n)
         name  (-> note (str/split #"\d") first keyword)
         nums  (last (str/split note #"\D"))
@@ -87,42 +67,19 @@
      :time     time
      :pitch    pitch}))
 
-
 (defn read-score
   "takes in a 'score' which is an array with a key-signature (or general config) as the first eleent and a series of
   keyword notes which contain note name, octave, and duration.  Transforms these into pitch and time info to be
   passed into 'play-score fn"
   [score]
   (let [time-sig (first score)
-        notes    (filterv #(not (contains? #{:4-4 :3-4} %)) score)
+        notes    (filterv #(not (contains? #{1 3/4} %)) score)
         inst     p/piano]
     (reset! cur-time nil)
     (reduce
      (fn [acc n]
        (case n
          (| ||) acc
-         (let [note (make-note2 n acc)]
-           (reset! cur-time (:time note))
-           (conj acc
-                 (merge note 
-                        {:time-signature time-sig
-                         :tempo          TEMPO})))))
-     []
-     notes)))
-
-(defn read-score2
-  "takes in a 'score' which is an array with a key-signature (or general config) as the first eleent and a series of
-  keyword notes which contain note name, octave, and duration.  Transforms these into pitch and time info to be
-  passed into 'play-score fn"
-  [score]
-  (let [time-sig (first score)
-        notes    (filterv #(not (contains? #{4/4 3/4} %)) score)
-        inst     p/piano]
-    (reset! cur-time nil)
-    (reduce
-     (fn [acc n]
-       (case n
-         (:| :||) acc
          (let [note (make-note n acc)]
            (reset! cur-time (:time note))
            (conj acc
@@ -141,18 +98,18 @@
    []
    (if (map? (first notes))
      notes
-     (read-score2 notes))))
+     (read-score notes))))
 
 (defn transpose
   "Adjusts the octave data of each note up or down n octaves"
   [score octaves dir]
   (reduce (fn [acc n]
             (if (and (<= 3 (count (name n)))
-                     (not (contains? #{:| :||} n)))
+                     (not (contains? #{'| '||} n)))
                (let [note     (make-note n [])
                      sn       (name n)
                      txp      (max ((if (= :up dir) + -) (:range note) octaves) 0)
-                     new-note (keyword (str (name (:name note)) txp (:rhythm note)))]
+                     new-note (str (name (:name note)) txp (:rhythm note))]
                  (conj acc new-note))
                (conj acc n)))
           (conj [] (first score))
@@ -166,13 +123,13 @@
   '(4/4 A02 B02 C12 B02))
 
 (def chordy1
-  [:4-4 :A31 :G31 :F31 :E31])
+  '(4/4 A31 G31 F31 E31))
 
 (def chordy2
-  [:4-4 :E21 :F21 :E21 :D21])
+  '(4/4 E21 F21 E21 D21))
 
 (def chordy3
-  [:4-4 :C21 :D21 :A21 :B21])
+  '(4/4 C21 D21 A21 B21))
 
 (do
 (play-notes chordy1)
@@ -183,22 +140,17 @@
 
 (play-notes bassy)
 
-(def score2
-  [:4-4 :E38 :D#38 :E38 :D#38 :E38 :B28 :D38 :C38 :A28 :||])
-
 (def score3
-  [:4-4
-   :A116 :C216 :B216 :A116 :|
-   :A116 :C216 :B216 :A116 :|
-   :A116 :C216 :B216 :A116 :|
-   :A116 :C216 :B216 :A116 :|
-   ])
+  '(4/4
+    A116 C216 B216 A116 |
+    A116 C216 B216 A116 |
+    A116 C216 B216 A116 |
+    A116 C216 B216 A116 |
+    ))
 
-(def score
-  [:4-4 :C34 :D34 :E34 :D34 :| :E38 :D38 :C38 :B28 :A24 :B24 :||])
-  ;; [:4-4 :C34 :D34 :E34 :D34 :| :C38 :C34 :C38 :C34 :||])
 (comment
-  (read-score score)
+  (read-score score3)
+  (play-notes score3)
 
   (s/mooger 32)
 
@@ -212,10 +164,6 @@
      (transpose score3 1 :down)
      (transpose score3 2 :down)
      ))
-
-  (map #(p/piano %) (range 25 49 12))
-
-  (for [i (range 9 21)] (at (+ (o/now) (* i (rand 2) 50)) (p/piano i)))
 
   (p/piano 23  0.01)
 
@@ -258,18 +206,18 @@
   (o/stop-all)
 
   (def minuet-in-c-bass
-    [:4-4
-     :C14 :C14 :C14 :C14     :|
-     :G#04 :G#04 :G#04 :G#04 :|
-     :F04 :F04 :F04 :F04     :|
-     :G04 :G04 :G04 :G04     :||])
+    '[4/4
+      C14 C14 C14 C14     |
+      G#04 G#04 G#04 G#04 |
+      F04 F04 F04 F04     |
+      G04 G04 G04 G04     ||])
 
   (def minuet-in-c-melody
-    [:4-4
-     :R04 :C52 :R08 :B416 :C516                 :|
-     :D58 :C58 :Bb48 :Ab48 :F#48 :G48 :Ab48     :|
-     :R04 :G58 :F58 :Eb58 :D58 :C58 :B48 :Ab48  :|
-     :C68 :Bb58 :Ab58 :G58 :F58 :Eb58 :D58 :C58 :||])
+    '[4/4
+      R04 C42 R08 B316 C416              |
+      D48 C48 Bb38 Ab38 F#38 G38 Ab38    |
+      R04 G48 F48 Eb48 D48 C48 B38 Ab38  |
+      C58 Bb48 Ab48 G48 F48 Eb48 D48 C48 ||])
 
   [(play-notes (flatten (repeat 2 minuet-in-c-bass)))
    (play-notes (flatten (repeat 2 minuet-in-c-melody)))]
